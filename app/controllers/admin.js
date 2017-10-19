f756f978e4862b260ce99e559cdcbd9c9841e110
DESKTOP-SIIJPEP\Kanglouis <kang@adquestasia.com>
2017-10-19 18:16:13 +0700

    admin controller

---
+++ app/controllers/admin.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+  router = express.Router(),
+  mongoose = require('mongoose'),
+  Article = mongoose.model('Article');
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/login1', function(req, res, next){
+  res.render('login', {
+      title: 'Login'
+    });
+});
+
+router.get('/1', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'Generator-Express MVC',
+      articles: articles
+    });
+  });
+});
