4199036b9b29a08c0bbcabe9f10f9f90ec0a220e
KhoaPham <dk_fallenangel_2111@yahoo.com>
2017-11-18 19:03:20 +0700

    Profile html + Fix html/css

--- public/web/js/main.js
+++ public/web/js/main.js
@@ -13,3 +13,19 @@ function setLoveHeight(){
 		$(this).height(width);
 	});
 }
+
+function test(obj){
+	alert($(obj).attr('data-toggle'))
+}
+
+function setLoveHeight(){
+	var avatar = $(".avatar_list .avatar");
+	avatar.each(function(){
+		var width=$(this).width();
+		$(this).height(width);
+	});
+}
+
+function test(obj){
+	alert($(obj).attr('data-toggle'))
+}
