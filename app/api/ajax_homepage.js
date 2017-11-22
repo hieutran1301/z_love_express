d598e8924b9319a04e83870f6dac919fad6d3944
DESKTOP-SIIJPEP\Kanglouis <doanhuu.lehuan@gmail.com>
2017-11-22 18:05:56 +0700

    profile ajax load

--- app/api/ajax_homepage.js
+++ app/api/ajax_homepage.js
@@ -7,3 +7,14 @@ var user = mongoose.model('zlove_users');
 module.exports = function (app, passport) {
     app.use('/ajax-home', router);
 };
+
+router.post('/getuserbyid', function(req, res, next){
+    var userid = req.body.id;
+    user.findOne({_id: userid}, function(err, data){
+        if (err) throw err;
+        if(!data) res.sendStatus(404)
+        else{
+            res.send(data);
+        }
+    });
+});
--- public/web/js/profile.js
+++ public/web/js/profile.js
@@ -1,12 +1,66 @@
-var AJAX_PATH = "";
+var AJAX_PATH = "../../../ajax-home/getuserbyid";
+
+var firstname 		= '',
+	lastname  		= '',
+	gender			= 1,
+	birthday 		= '',
+	currplace		= 1,
+	relationship 	= 0,
+	working 		= '',
+	workingplace    = 'University of Information Technology';
+
+var USERID = '';
+
+var csrf = $('input[name=_csrf]').val();
+if (csrf != null || csrf != undefined) {
+	$.ajaxSetup({
+		headers: {'X-CSRF-Token': csrf}
+	});
+}
 
 $(document).ready(function(){
+	//get userid
+	USERID = $('input[name=_userid]').val();
 
-    var csrf = $('input[name=_csrf]').val();
-	if (csrf != null || csrf != undefined) {
-		$.ajaxSetup({
-			headers: {'X-CSRF-Token': csrf}
-		});
-	}
+	//fetch info
+	fetchInfo(USERID);
 
 });
+
+function fetchInfo(id){
+	$.post(AJAX_PATH, 
+		{
+			id: id
+		}, 
+		function(data){
+			if (data != '' && data != 404) printInfo({
+				firstname		: data.FirstName,
+				lastname		: data.LastName,
+				gender			: data.Gender,
+				birthday 		: data.DateOfBirth,
+				currplace		: data.CurrentPlace,
+				relationship 	: 0,
+				working 		: data.Working,
+				workingplace    : 'University of Information Technology'
+			});
+		}
+	);
+}
+
+function printInfo(data){
+	var txtFullName = $('#FullName');
+	var txtGender 	= $('#Gender');
+	var txtBirthday	= $('#Birthday');
+	var txtPlace 	= $('#Place');
+	var txtRela 	= $('#Relationship');
+	var txtWorking 	= $('#Working');
+	var txtWkPlace 	= $('#WorkingPlace');
+
+	txtFullName.html(data.firstname+' '+data.lastname);
+	if (data.gender == 1) {txtGender.html("Nam")} else txtGender.html("Nữ");
+	txtBirthday.html(data.birthday);
+	txtPlace.html(data.currplace);
+	if (data.relationship == 0) {txtRela.html('Độc thân')} else {txtRela.html('Đang hẹn hò')};
+	txtWorking.html(data.working);
+	txtWkPlace.html(workingplace);
+}
