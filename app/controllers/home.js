e6173c4364ae6dffd9bd48d5687cf5d0191ea894
DESKTOP-SIIJPEP\Kanglouis <doanhuu.lehuan@gmail.com>
2017-11-23 15:46:38 +0700

    View profile

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -61,6 +61,37 @@ router.get('/profile-new', function(req, res, next){
         title: 'Profile',
         csrf: req.csrfToken(),
         userid: req.session.homeuserid,
+        self: true,
+        data: {
+          fullname    : data.FirstName+' '+data.LastName,
+          age         : data.getAge(),
+          birthday    : data.DateOfBirth,
+          relation    : "Độc thân",
+          gender      : data.getGender(),
+          crrPlace    : data.getCity(data.CurrentPlace),
+          working     : data.Working,
+          workingat   : "ĐH Công nghệ thông tin - ĐHQG HCM"
+        }
+      });
+    }
+  });
+});
+
+router.get('/profile-new/:username', function(req, res, next){
+  var username = req.params.username;
+  var self     = false;
+  user.findOne({Username: username}, function(err, data){
+    if (err) throw err;
+    if (!data){
+      res.redirect('/home/');
+    }
+    else{
+      if (data._id == req.session.homeuserid) self = true;
+      res.render('web/pages/profile_new', {
+        title: 'Profile',
+        csrf: req.csrfToken(),
+        userid: req.session.homeuserid,
+        self: self,
         data: {
           fullname    : data.FirstName+' '+data.LastName,
           age         : data.getAge(),
