ade24c221b7a05d9657806f76ea651f9c9c7f219
HieuDepTrai\trant <tranhieu130196@gmail.com>
2017-12-22 12:05:37 +0700

    API app

--- app/api/general.js
+++ app/api/general.js
@@ -8,16 +8,50 @@ module.exports = function (app, passport) {
   app.use('/app-api', router);
 };
 
-router.get('/exist/:username', async (req, res, next) => {
-	let username = req.params.username;
-	try {
-		let query = await user.findOne({Username: username});
-		if (query) { res.send("Username is unavailable!") }
-		else{
-			res.status(404).send("Username is available!");
-		}
-	} catch (err) {
-		console.log(err);
-		res.sendStatus(404);
-	}
+router.get('/exist/:username/:password', function (req, res, next) {
+  var username = req.params.username;
+  var password = req.params.password;
+
+  console.log(username);
+  console.log(password);
+
+
+  user.findOne({Username: username}, function (err, data) {
+    //if data == null => user not found
+    if (!data) {
+      res.status(404);
+      return 0;
+    } else {
+      if (!data.validPassword(password)) {
+        res.status(404);
+      } else {
+        res.send("Login success.")
+      }
+    }
+  });
+});
+
+router.get('/create/:firstName/:lastName/:username/:mail/:password', function(req, res){
+  var firstName = req.params.firstName;
+  var lastName  = req.params.lastName;
+  var email     = req.params.email;
+  var username  = req.params.username;
+  var password  = req.params.password;
+
+  console.log(firstName);
+
+  user.findOne({Username: username}, function (err, data) {
+    //if data == null => user not found
+    if (!data) {
+      res.status(404);
+      return 0;
+    } else {
+      if (!data.validPassword(password)) {
+        res.status(404);
+      } else {
+        res.send("Login success.")
+      }
+    }
+  });
+  
 });
