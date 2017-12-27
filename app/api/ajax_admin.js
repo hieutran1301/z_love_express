bd03b50f2eb89cc1381b160216b3e6906599d236
DESKTOP-SIIJPEP\Kanglouis <doanhuu.lehuan@gmail.com>
2017-12-27 17:13:23 +0700

    Admin@Post Manager is done, testing

--- app/api/ajax_admin.js
+++ app/api/ajax_admin.js
@@ -318,7 +318,7 @@ router.post('/post', async function(req, res, next){
 	if (option == 'fetchPostsPending'){
 		try {
 			var arrData = [];
-			let data = await post.find({Status: 0}).sort({created_at: -1}).exec();
+			let data = await post.find().sort({created_at: -1}).exec();
 			for (var i = 0; i<data.length; i++){
 				var id = data[i]._id;
 				var Title =  data[i].Title;
@@ -359,4 +359,225 @@ router.post('/post', async function(req, res, next){
 			res.status(500).send('Server Error');
 		}
 	}
+
+	if (option == 'getPostById'){
+		try {
+			var postID = req.body.postID;
+			post.findOne({_id: postID}, function(err, post){
+				if (post){
+					var CreatedByID = post.CreatedBy;
+					user.findOne({_id: CreatedByID}, function(err, usr){
+						var Username = usr.Username;
+						var PostID 	= post._id;
+						var Title 	= post.Title;
+						var FromAge = post.FromAge;
+						var ToAge	= post.ToAge;
+						var Desc 	= post.Description;
+						var Job		= post.Job;
+						var City	= post.City;
+						var Target  = post.Target;
+						var Status	= post.Status;
+						res.send({
+							_id: PostID,
+							Title: Title,
+							Description: Desc,
+							FromAge: FromAge,
+							ToAge: ToAge,
+							Job: Job,
+							City: City,
+							CreatedBy: Username,
+							Target: Target,
+							Status: Status
+						});
+					});
+				}
+				else{
+					res.status(404).send('Not found');
+				}
+			});
+		} catch (error) {
+			console.log('ERROR@AJAX_ADMIN:POST: '+error);
+			res.status(500).send('Server Error');
+		}
+	}
+
+	if (option == 'addNewPost'){
+		var Title = req.body.title;
+		var FromAge =  (req.body.fromage)*1;
+		var ToAge = (req.body.toage)*1;
+		var Description = req.body.description;
+		var Target = (req.body.target)*1;
+		var Job = (req.body.job)*1;
+		var City = (req.body.city)*1;
+		var CreatedBy = req.body.createdby;
+		var Status = (req.body.status)*1;
+		console.log(CreatedBy);
+		console.log(Job);
+
+		var now  = new Date();
+		var CreatedDate = ''+addZero(now.getHours())+':'+addZero(now.getMinutes())+':'+addZero(now.getSeconds())+' '+now.getDate()+'/'+(now.getMonth()+1)+'/'+now.getFullYear(); 
+		
+		user.findOne({Username: CreatedBy}, function(err, data){
+			if (err) console.log(err);
+			if (!data){
+				res.status(200).send({
+					status: 'error',
+					msg: 'Username not exist'
+				});
+			}
+			else{
+				post.findOne({CreatedBy: data._id}, function(err, result){
+					if (result){
+						res.status(200).send({
+							status: 'error',
+							msg: 'One post allow for this user'
+						});
+					}
+					else{
+						let newPost = new post({
+							"Title"   : Title,
+							"Target"  : Target,
+							"FromAge" : FromAge,
+							"ToAge"   : ToAge,
+							"City"    : City,
+							"Job"     : Job,
+							"Description" : Description,
+							"CreatedDate" : CreatedDate,
+							"CreatedBy"   : data._id,
+							"EditedDate"  : CreatedDate,
+							"View"        : 0,
+							"NumberApply" : 0,
+							"Status"      : Status
+						});
+		
+						newPost.save(function(err, result){
+							if (err) console.log(err);
+							if (!result._id){
+								res.status(200).send({
+									status: 'error',
+									msg: 'Cannot save this post!'
+								});
+							}
+							else{
+								res.status(200).send({
+									status: 'success',
+									msg: 'Cannot save this post!'
+								});
+							}
+						});
+					}
+				});
+			}
+		});
+	}
+
+	if (option == 'editPost'){
+		var Title = req.body.title;
+		var FromAge =  (req.body.fromage)*1;
+		var ToAge = (req.body.toage)*1;
+		var Description = req.body.description;
+		var Target = (req.body.target)*1;
+		var Job = (req.body.job)*1;
+		var City = (req.body.city)*1;
+		var CreatedBy = req.body.createdby;
+		var Status = (req.body.status)*1;
+		var postID = req.body.postID;
+		console.log(CreatedBy);
+		console.log(Job);
+
+		var now  = new Date();
+		var CreatedDate = ''+addZero(now.getHours())+':'+addZero(now.getMinutes())+':'+addZero(now.getSeconds())+' '+now.getDate()+'/'+(now.getMonth()+1)+'/'+now.getFullYear(); 
+		
+		post.findOne({_id: postID}, function(err, _post){
+			if (err) console.log(err);
+			if (!_post){
+				res.status(200).send({
+					status: 'error',
+					msg: 'Post not found'
+				});
+			}
+			else{
+				user.findOne({Username: CreatedBy}, function(err, data){
+					if (err) console.log(err);
+					if (!data){
+						res.status(200).send({
+							status: 'error',
+							msg: 'Username not exist'
+						});
+					}
+					else{
+						post.findOne({CreatedBy: data._id}, function(err, result){
+							if (result){
+								if (result._id != postID){
+									res.status(200).send({
+										status: 'error',
+										msg: 'One post allow for this user'
+									});
+								}
+								else{
+									post.updateOne({_id: postID}, {
+										"Title"   : Title,
+										"Target"  : Target,
+										"FromAge" : FromAge,
+										"ToAge"   : ToAge,
+										"City"    : City,
+										"Job"     : Job,
+										"Description" : Description,
+										"CreatedDate" : CreatedDate,
+										"CreatedBy"   : data._id,
+										"EditedDate"  : CreatedDate,
+										"Status"      : Status
+									}, function(err, updatedPost){
+										if (err) console.log(err);
+										res.status(200).send({
+											status: 'success',
+											msg: 'success'
+										});
+									});
+								}
+							}
+							else{
+								post.updateOne({_id: postID}, {
+									"Title"   : Title,
+									"Target"  : Target,
+									"FromAge" : FromAge,
+									"ToAge"   : ToAge,
+									"City"    : City,
+									"Job"     : Job,
+									"Description" : Description,
+									"CreatedDate" : CreatedDate,
+									"CreatedBy"   : data._id,
+									"EditedDate"  : CreatedDate,
+									"Status"      : Status
+								}, function(err, updatedPost){
+									if (err) console.log(err);
+									res.status(200).send({
+										status: 'success',
+										msg: 'success'
+									});
+								});
+							}
+						});
+					}
+				});
+			}
+		});
+	}
+
+	if (option == 'removePost'){
+		var postID = req.body.postID;
+		post.findOne({_id: postID}).remove(function(){
+			res.status(200).send({
+				status: 'success',
+				msg: 'success'
+			});
+		});
+	}
 });
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
